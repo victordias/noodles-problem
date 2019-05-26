a56da8df858f473795585c56b093a0eebedc7485
Victor Dias de Oliveira <victor.dias.oliveira@gmail.com>
2019-05-26 19:02:28 +0000

    upload problem to github

---
+++ config/messages.js
@@ -0,0 +1,6 @@
+module.exports = {
+  USAGE: "USAGE: node bin/miojo <tempo de cozimento> <ampuleta> <ampuleta> - Todos valores devem ser número Inteiro",
+  AMP_TIME_CANNOT_BE_LESS_THEN_COOKING_TIME: 'Oops! Tempo das ampuletas não pode ser menor que o tempo de cozimento.',
+  CANNOT_COOK_WITH_THESE_TIMES: 'Oops! Não é possivel obter o tempo de cozimento exato com as ampulhetas informadas.',
+  MAX_ATTEMPTS_REACHED: 'Game Over! Não foi possivel resolver com o número maximo de tentativas configurado.'
+}
